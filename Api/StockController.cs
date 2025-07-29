a40852dc5c1e03610701f351ef6906590de45d5e
Amir Golmoradi <LeoQueenzer2.0@gmail.com>
2025-07-29 20:12:34 +0330

    Adds CRUD API for stock management
    Introduces a new StockController with endpoints to handle stock-related operations including retrieval, creation, updating, and deletion. Implements integration with the database and DTO mappings to streamline data handling.
    
    Provides a RESTful API structure to support stock management functionality.

---
+++ Api/StockController.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockMarket.Data;
+using StockMarket.Dto.Stock;
+using StockMarket.Interface;
+using StockMarket.Mappers;
+
+namespace StockMarket.Api;
+
+[ApiController]
+[Route("api/stock")]
+public class StockController(AppDBContext appDbContext, IStockRepository stockRepo) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var stocks = await stockRepo.GetAllAsync();
+        var stockDto = stocks.Select(s => s.ToStockDto());
+        return Ok(stocks);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById([FromRoute] int id)
+    {
+        var stock = await appDbContext.Stocks.FindAsync(id);
+
+        if (stock == null) return NotFound();
+
+        return Ok(stock.ToStockDto());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateStockRequestDto stockDto)
+    {
+        // Create our stock model
+        var stockModel = stockDto.ToStockFromCreateDto();
+        // Check if Stock is Available
+        if (stockModel.Equals(null)) return BadRequest();
+        await appDbContext.Stocks.AddAsync(stockModel);
+        await appDbContext.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel);
+    }
+
+    [HttpPut()]
+    [Route("{id:int}")]
+    public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+    {
+        // 1. Find Existing Stocks
+        var stockModel = await appDbContext.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+        if (stockModel == null) return NotFound();
+        // 2. Update the Stocks that we want
+        stockModel.Symbol = updateDto.Symbol;
+        stockModel.CompanyName = updateDto.CompanyName;
+        stockModel.Industry = updateDto.Industry;
+        stockModel.Purchase = updateDto.Purchase;
+        stockModel.LastDiv = updateDto.LastDiv;
+        // 3. Save all the changes.
+        await appDbContext.SaveChangesAsync();
+        return Ok(stockModel.ToStockDto());
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public async Task<IActionResult> Delete([FromRoute] int id)
+    {
+        // 1. Find Existing Stocks
+        var stockModel = await appDbContext.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+        if (stockModel == null) return NotFound();
+
+        // 2. Delete the Stocks that we want
+        appDbContext.Stocks.Remove(stockModel);
+
+        // 3. Save all the changes.
+        await appDbContext.SaveChangesAsync();
+
+        return Ok(stockModel.ToStockDto());
+    }
+}
